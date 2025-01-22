1295e8011181f66e5693af954fe905a7691a4f56
gwuzzy <gwuzzy@users.noreply.github.com>
2025-01-22 02:16:33 -0500

    add script to remove empty paragraphs from generated icml

---
+++ .github/node-fixup-icml/index.js
@@ -0,0 +1,31 @@
+// @ts-check
+import { readFile, writeFile, readdir } from "fs/promises";
+import * as cheerio from "cheerio";
+
+const ICML_DIR = "content-indesign";
+
+for (const lang of ["en", "fr"]) {
+  const dir = `${ICML_DIR}/${lang}`;
+  const files = await readdir(dir);
+
+  for (const file of files) {
+    if (!file.endsWith(".icml")) continue;
+
+    const path = `${dir}/${file}`;
+    const doc = await readFile(path);
+    const $ = cheerio.load(doc, { xml: { decodeEntities: false } });
+
+    // remove empty paragraphs
+    const emptyParagraphs = $("ParagraphStyleRange")
+      .toArray()
+      .filter((para) => $(para).text().trim().length === 0);
+
+    if (emptyParagraphs.length > 0) {
+      console.log(
+        `Doc "${path}" has ${emptyParagraphs.length} empty paragraphs. Cleaning up.`
+      );
+      for (const e of emptyParagraphs) $(e).remove();
+      await writeFile(path, $.xml());
+    }
+  }
+}
